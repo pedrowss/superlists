b847194ecc45fcb1410bc9bf4532d771b019bf34
pedro siqueira <pedrowss@gmail.com>
2020-03-17 23:14:30 -0300

    Basic view now returns minimal HTML

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import re_path
 from lists import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home'),
+    re_path(r'^$', views.home_page, name='home'),
 ]
