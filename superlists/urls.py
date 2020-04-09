c4009e3010fac958bb094dd45d57b9d29ddf0ecc
pedro siqueira <pedrowss@gmail.com>
2020-04-09 17:50:53 -0300

    Cut down superlists/urls.py

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,24 +1,8 @@
-"""superlists URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf.urls import re_path
-from lists import views
+from django.conf.urls import include, url
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    re_path(r'^$', views.home_page, name='home'),
-    re_path(r'^lists/new$', views.new_list, name='new_list'),
-    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/', include(list_urls)),
 ]
