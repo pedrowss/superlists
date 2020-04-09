f97ee872595c8f20cfad06b9e93bdeb2d2c34a81
pedro siqueira <pedrowss@gmail.com>
2020-04-09 11:57:27 -0300

    Switch url to using back-d capture group

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,5 +19,5 @@ from lists import views
 urlpatterns = [
     re_path(r'^$', views.home_page, name='home'),
     re_path(r'^lists/new$', views.new_list, name='new_list'),
-    re_path(r'^lists/(.+)/$', views.view_list, name='view_list'),
+    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
 ]
